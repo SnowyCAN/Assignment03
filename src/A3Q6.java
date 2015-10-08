87b01906f48e09340a15afe83f590dd1fd0609e9
tituo4996 <tituo4996@HRH-IC0027340.wrdsb.ca>
2015-10-08 11:19:09 -0400

    Saving to the cloud

---
+++ src/A3Q6.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tituo4996
+ */
+public class A3Q6 {
+    
+    public void bucketSort(int[] numbers)
+    {
+        
+    }
+    
+    public static void main(String[] args) {
+        int[] numbers=new int[101];
+        
+        for (int i = 0; i < 10; i++) {
+            numbers[i]=(int)Math.floor(Math.random()*101);
+        }
+    }
+}
